03622adfc025385aa841d7b98a87cbef7466bd03
Akhil <83524932+Akhil2k01@users.noreply.github.com>
2026-03-14 19:24:22 +0530

    Replace TopologyHelper with TaskHelper for task state

--- middle_layer.py
+++ middle_layer.py
@@ -1,11 +1,11 @@
 import logging
 from flask_restful import Resource
-from topology import TopologyHelper
+from mongo_task_state import TaskHelper
 
 class MiddleLayer(Resource):
     def post(self):
         try:
-            res = TopologyHelper.split_and_store_topology_cm(topology_uuid="123", username="test_user", model_name="test_model", execution_uuid="", is_prod_execution="false")
+            res = TaskHelper.store_mongo_task_state(topology_uuid="123", username="test_user", model_name="test_model", execution_uuid="", is_prod_execution="false")
             print(f"Response from split_and_store_topology_cm: {res}")
             return {'message': 'Scheduled an execution for the redis: {}'.format("123"),'statusCode': 200}
         except Exception as err:
